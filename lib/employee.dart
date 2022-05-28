c44d07120effbc7a488dae53f027c1e0c3faf368
hemant Kumar <hemantkumar692@gmail.com>
2022-05-28 18:15:03 +0530

    flutter hive database example

---
+++ lib/employee.dart
@@ -0,0 +1,20 @@
+
+import 'package:hive/hive.dart';
+
+part 'employee.g.dart';
+
+@HiveType(typeId: 0)
+class Employee{
+
+  @HiveField(0)
+  String empName;
+
+  @HiveField(1)
+  String empSalary;
+
+  @HiveField(2)
+  String empAge;
+
+  Employee({required this.empName, required this.empSalary, required this.empAge});
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
+
+import 'EmployeesListScreen.dart';
+import 'employee.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  Directory directory = await getApplicationDocumentsDirectory();
+  Hive.init(directory.path);
+  Hive.registerAdapter(EmployeeAdapter());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home:  EmployeesListScreen(),
+    );
+  }
+}
+
